a339810b75c74b22bf2030364d89c6e55d3c468c
Kevin Townsend <microbuilder@users.noreply.github.com>
2015-12-03 11:56:31 +0100

    Disabled dtostrf on STM32F2

--- Adafruit_MQTT.cpp
+++ Adafruit_MQTT.cpp
@@ -21,7 +21,7 @@
 // SOFTWARE.
 #include "Adafruit_MQTT.h"
 
-#if defined(ARDUINO_SAMD_ZERO) || defined(ARDUINO_STM32F2_FEATHER)
+#ifdef ARDUINO_SAMD_ZERO
 static char *dtostrf (double val, signed char width, unsigned char prec, char *sout) {
   char fmt[20];
   sprintf(fmt, "%%%d.%df", width, prec);
