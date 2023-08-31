1091b17df74f9f69b2961cc69fa6289da7181d3e
brentru <brent@adafruit.com>
2023-08-31 13:34:29 -0400

    make more informative

--- Adafruit_MQTT.h
+++ Adafruit_MQTT.h
@@ -107,7 +107,7 @@
 // Largest full packet we're able to send.
 // Need to be able to store at least ~90 chars for a connect packet with full
 // 23 char client ID.
-// Future TODO: This should be replaced by the ability to
+// Future TODO: This should be replaced by the ability to dynamically allocate a buffer as needed.
 #if defined(ARDUINO_ARCH_ESP8266) || defined(ARDUINO_ARCH_ESP32) ||            \
     defined(ARDUINO_ARCH_RP2040) || defined(ARDUINO_ARCH_SAMD)
 #define MAXBUFFERSIZE (512)
